477d0d1fdb266a3e8a6e7121d0f203c597ef1cf6
Ahmet ÇAKIR <ahmet.ckr720@gmail.com>
2021-12-29 21:02:42 +0300

    Başlangıç
    index.php ve ayar.css eklendi

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>
+		<link rel="stylesheet" type="text/css" href="ayar.css">
+	</title>
+</head>
+<body>
+	<div class="ust">
+		
+	</div>
+
+	<div class="menu">
+		
+	</div>
+
+	<div class="icerik">
+		
+	</div>
+
+	<div class="footer">
+		
+	</div>
+</body>
+</html>
