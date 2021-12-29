3e11da5de8d47c24576e39baca51f63368729946
Ahmet ÇAKIR <ahmet.ckr720@gmail.com>
2021-12-29 21:36:24 +0300

    menu eklendi
    menü eklendi

--- index.php
+++ index.php
@@ -4,8 +4,10 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="ayar.css">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Cormorant" />
+    <link href="cssmenu/cssmenu.css" rel="stylesheet" />
+	
 	<title>
-		
 	</title>
 </head>
 <body>
@@ -14,9 +16,43 @@
 		<b>Youtube Kanalı</b>
 	</div>
 
-	<div class="menu">
-		
+	<div id="menuDemo">
+<!--start CssMenu-->
+<div class="menu-icon-wrapper">
+	<div class="menu-icon" onclick="toggleCssMenu(this)">
+		<div class="three-line">
+			<span></span>
+			<span></span>
+			<span></span>
+		</div>
 	</div>
+</div>
+<div id="cssmenu">
+    <ul>
+        <li><a href="#">Portfolio</a></li>
+        <li><a href="#">Creative</a></li>
+        <li><a href="#">Studio</a></li>
+        <li><a href="#">Press</a></li>
+        <li><a href="#">Contact</a></li>
+    </ul>
+</div>
+<script>
+    function toggleCssMenu(icon) {
+        var cssmenu = document.getElementById('cssmenu');
+        if (icon.className.indexOf('active') == -1) {
+            icon.className = 'menu-icon active';
+			cssmenu.style.display = "block";
+			setTimeout(function(){cssmenu.className = 'active';},0);            
+        }
+        else {
+            icon.className = 'menu-icon';
+			cssmenu.className = '';
+			setTimeout(function(){cssmenu.style.display = "none";},411); 
+        }
+    }
+</script>
+<!--end CssMenu-->
+</div>
 
 	<div class="icerik">
 		
