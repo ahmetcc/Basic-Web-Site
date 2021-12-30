f8dbcdd895931491257576d1d525878c75fd202d
Ahmet ÇAKIR <ahmet.ckr720@gmail.com>
2021-12-30 21:55:34 +0300

    footer eklendi

--- iletisim.php
+++ iletisim.php
@@ -1,3 +1,35 @@
-<h1>Iletişim</h1>
+<form action="" method="" >
+Adınız :<input type="text" name="txtIsim">
 
-<p>Burası iletişim sayfasıdır. Buradan iletişim bilgilerimizi görebilirsiniz.</p>
+<br><br>
+Soyadınız :<input type="text" name="txtSIsim">
+<br><br>
+E-posta :<input type="text" name="txtSIsim">
+<br><br>
+Konu :<select>
+<option>Öneri</option>
+<option>Şikayet</option>
+<option>Soru</option>
+</select>
+<br><br>
+
+
+Cinsiyetiniz :<input type="radio" name="cinsiyet">Erkek <input type="radio" name="cinsiyet">Bayan
+<br><br>
+
+Yaşadığınız İl :<select>
+<option>İstanbul</option>
+<option>Ankara</option>
+<option>İzmir</option>
+<option>Bursa</option>
+<option>Antalya</option>
+</select>
+<br><br>
+
+Mesajınız :<br>
+<textarea rows="5" cols="30"></textarea>
+<br><br>
+
+<input type="submit" name="btnGonder" value="Gönder">
+<input type="reset" name="btnTemizle" value="Temizle">
+</form>
